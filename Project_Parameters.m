5aa9400302943597c8ff39834fafb09b5e5a1814
Doncey Albin <donceyalbin@gmail.com>
2022-12-04 12:25:01 -0700

    Updated parameters to be just purely parameters

--- Project_Parameters.m
+++ Project_Parameters.m
@@ -14,53 +14,25 @@ format compact
 % clear all
 
 % Given data
-L = .5;
-dt = 10;
-phi_g = -5*pi/12:5*pi/12;
-vg = 3;
-wg = -pi/6: pi/6;
-va = 10:20;
-eta_g_nom = 10;
-zeta_g_nom = 0;
-theta_g_nom = pi/2;
-phi_g_nom = -pi/18;
-eta_a_nom = -60;
-zeta_a_nom = 0;
-theta_a_nom = -pi/2;
-va_nom = 12;
-wa_nom = pi/25;
-
-% Non-linear Continuous-Time Matrices
-A = [0 0 -vg*sin(theta_g_nom) 0 0 0; 
-     0 0  vg*cos(theta_g_nom) 0 0 0; 
-     0 0  0                   0 0 0; 
-     0 0  0 0 0 -va_nom*sin(theta_a_nom); 
-     0 0 0 0 0 va_nom*cos(theta_a_nom); 
-     0 0 0 0 0 0]; 
-B = [cos(theta_g_nom) 0 0 0; 
-     sin(theta_g_nom) 0 0 0; 
-     1/L*tan(phi_g_nom) vg/(L*cos(phi_g_nom)^2) 0 0; 
-     0 0 cos(theta_a_nom) 0; 
-     0 0 sin(theta_a_nom) 0; 
-     0 0 0 1];
+L = .5;                             %
+delta_t = 0.1;                      %
 
 
-C = [(zeta_a_nom-zeta_g_nom)/(((zeta_a_nom-zeta_g_nom)^2/(eta_a_nom-eta_g_nom)^2+1)*(eta_a_nom-eta_g_nom)^2) -1/((eta_a_nom-eta_g_nom)*((zeta_a_nom-zeta_g_nom)^2/(eta_a_nom-eta_g_nom)^2+1)) -1 -(zeta_a_nom-zeta_g_nom)/((eta_a_nom-eta_g_nom)^2*((zeta_a_nom-zeta_g_nom)^2/(eta_a_nom-eta_g_nom)^2+1)) 1/((eta_a_nom-eta_g_nom)*((zeta_a_nom-zeta_g_nom)^2/(eta_a_nom-eta_g_nom)^2+1)) 0; (eta_g_nom-eta_a_nom)/sqrt((eta_g_nom-eta_a_nom)^2+(zeta_g_nom-zeta_a_nom)^2) (zeta_g_nom-zeta_a_nom)/sqrt((zeta_g_nom-zeta_a_nom)^2+(eta_g_nom-eta_a_nom)^2) 0 -(eta_g_nom-eta_a_nom)/sqrt((eta_g_nom-eta_a_nom)^2+(zeta_g_nom-zeta_a_nom)^2) -(zeta_g_nom-zeta_a_nom)/sqrt((eta_g_nom-eta_a_nom)^2+(zeta_g_nom-zeta_a_nom)^2) 0; -(zeta_g_nom-zeta_a_nom)/((eta_g_nom-eta_a_nom)^2*((zeta_g_nom-zeta_a_nom)^2/(eta_g_nom-eta_a_nom)^2+1)) 1/((eta_g_nom-eta_a_nom)*((zeta_g_nom-zeta_a_nom)^2/(eta_g_nom-eta_a_nom)^2+1)) 0 (zeta_g_nom-zeta_a_nom)/(((zeta_g_nom-zeta_a_nom)^2/(eta_g_nom-eta_a_nom)^2+1)*(eta_g_nom-eta_a_nom)^2) -1/((eta_g_nom-eta_a_nom)*((zeta_g_nom-zeta_a_nom)^2/(eta_g_nom-eta_a_nom)^2+1)) 0; 0 0 0 1 0 0; 0 0 0 0 1 0];
-Ah = [A B; zeros(4,10)];
-expAh = expm(Ah*dt);
+phi_g_bounds = -5*pi/12:5*pi/12;    %
+v_g_max = 3;                        % Top ground speed of UGV
 
-% DT Matrices
-F = expAh(1:6,1:6)
-G = expAh(1:6, 7:10)
-H = C; 
-Ct = ctrb(F,G);
-rc = rank(Ct);
-eig_d = eig(F);
+v_a_bounds = 10:20;                 %
+omega_a_bounds = -pi/6: pi/6;       %
 
-%Dynamics are fully controllable, therefore the system should be
-%stabalizable, however because eigen values are positive, the system is
-%unstable
-Ob = obsv(F,H);
-ro = rank(Ob');
+% Nominal Operating Conditions
+eta_g_nom = 10;                     %
+zeta_g_nom = 0;                     %
+theta_g_nom = pi/2;                 %
+phi_g = -pi/18;                     % Constant input
+v_g = 2;                            % Constant input
 
-%Measurement equations are fully observable
+eta_a_nom = -60;                    %
+zeta_a_nom = 0;                     %
+theta_a_nom = -pi/2;                %
+v_a = 12;                           % Constant input
+omega_a = pi/25;                    % Constant input
