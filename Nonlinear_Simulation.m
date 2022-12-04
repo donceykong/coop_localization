bc1415cb0bbb99310bc5c5bd820f84eaadca494d
Doncey Albin <donceyalbin@gmail.com>
2022-12-04 13:34:14 -0700

    Updated Nonlinear simulation to also show simulated measurements

--- Nonlinear_Simulation.m
+++ Nonlinear_Simulation.m
@@ -46,3 +46,34 @@ plot(tn, x(5,:))
 
 subplot(6,1,6)
 plot(tn, wrapToPi(x(6,:)))
+
+% Calculate all Meaurements
+y = zeros(6,1001);
+
+column = 1;
+for time = delta_t:delta_t:100
+    y(1, column) = atan2(x(5, column) - x(2, column), x(4, column) - x(1, column)) - x(3, column);
+    y(2, column) = sqrt((x(1, column) - x(4, column))^2 + (x(2, column) - x(5, column))^2);
+    y(3, column) = atan2(x(2, column) - x(5, column), x(1, column) - x(4, column)) - x(6, column);
+    y(4, column) = x(4, column);
+    y(5, column) = x(5, column);
+
+    column = column + 1;
+end
+
+% Plot all the measurements
+figure
+subplot(5,1,1)
+plot (tn, wrapToPi(y(1, :)))
+
+subplot(5,1,2)
+plot (tn, y(2, :))
+
+subplot(5,1,3)
+plot (tn, wrapToPi(y(3, :)))
+
+subplot(5,1,4)
+plot (tn, y(4, :))
+
+subplot(5,1,5)
+plot (tn, y(5, :))
